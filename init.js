793113b75a60d700f12c273d7a8467baf4197d26
Suhail Gatech <suhail.gupta@gatech.edu>
2018-06-19 12:51:10 +0000

    adds nodejs file as a wrapper to python

---
+++ init.js
@@ -0,0 +1,82 @@
+const EventEmitter = require('events').EventEmitter;
+const { spawn } = require('child_process');
+
+const Detector = (() => {
+
+    const _CORE_ENV = "python";
+    const _CORE_LIB = "/home/suhail.gupta/models/research/object_detection/logodetect.py";
+    const _CORE_LIB_PARAMS = "";
+
+    const _EVENT_MAP = {
+        DATA: 'cnn-response',
+        ERR: 'cnn-error',
+        END: 'cnn-end'
+    };
+
+    function _createProbList(response) {
+        try {
+            let probList = JSON.parse(response)
+            return { probList };
+        } catch (err) {
+            return { err };
+        }
+    }
+
+    return class LogoDetector extends EventEmitter {
+
+        constructor() {
+            super();
+            this.CORE_ENV = _CORE_ENV;
+            this.CORE_LIB = _CORE_LIB;
+            this.CORE_LIB_PARAMS = _CORE_LIB_PARAMS;
+        }
+
+        coldBoot() {
+            try {
+                let args = [
+                    this.CORE_LIB,
+                    ...this.CORE_LIB_PARAMS.split(' ')
+                ];
+
+                const spawnedCNN = spawn(this.CORE_ENV, args);
+
+                spawnedCNN.stdout.on('data', (data) => {
+                    let { probList, err } = _createProbList(data.toString());
+                    if (probList)
+                        this.emit(_EVENT_MAP.DATA, probList);
+                    else
+                        this.emit(_EVENT_MAP.ERR, err);
+                });
+
+                spawnedCNN.stderr.on('data', (data) => {
+                    this.emit(_EVENT_MAP.ERR, data);
+                });
+
+                spawnedCNN.on('close', (code) => {
+                    this.emit(_EVENT_MAP.END, { code });
+                });
+            } catch (err) {
+                this.emit(_EVENT_MAP.ERR, err);
+            }
+        }
+
+    }
+})();
+
+
+module.exports = Detector;
+
+let cnn = new Detector();
+cnn.coldBoot();
+
+cnn.on('cnn-response', (data) => {
+    console.log(data);
+})
+
+cnn.on('cnn-error', (data) => {
+    console.log(data);
+});
+
+cnn.on('cnn-end', (data) => {
+    console.log(data);
+});
