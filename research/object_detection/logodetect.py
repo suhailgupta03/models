c27c968893b63cec45a9aad2ed13c29a1defff0b
Anurag Singh <anurag.singh.phy15@itbhu.ac.in>
2018-07-11 14:28:38 +0530

    Update logodetect.py

--- research/object_detection/logodetect.py
+++ research/object_detection/logodetect.py
@@ -160,21 +160,25 @@ def run_inference_for_images(graph):
                         if tensor_name in all_tensor_names:
                             tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
                                 tensor_name)
-                    if 'detection_masks' in tensor_dict:
-                        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-                        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-                        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-                        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-                        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
-                        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
-                        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-                            detection_masks, detection_boxes, image.shape[0], image.shape[1])
-                        detection_masks_reframed = tf.cast(
-                            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-                        # Follow the convention by adding back the batch dimension
-                        tensor_dict['detection_masks'] = tf.expand_dims(
-                            detection_masks_reframed, 0)
-                    image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+		    try:	
+			    if 'detection_masks' in tensor_dict:
+				detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+				detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+				# Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+				real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+				detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+				detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+				detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+				    detection_masks, detection_boxes, image.shape[0], image.shape[1])
+				detection_masks_reframed = tf.cast(
+				    tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+				# Follow the convention by adding back the batch dimension
+				tensor_dict['detection_masks'] = tf.expand_dims(
+				    detection_masks_reframed, 0)
+			    image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+		    except:
+			    os.remove(os.path.join(PATH_TO_TEST_IMAGES_DIR,image_path))
+			  
 
                     # Run inference
                     output_dict = sess.run(tensor_dict,
